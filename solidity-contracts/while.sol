364dae0e982fd3fe5f8ba1eed0843c3d428ab190
Andrey Sobol <baskerville@andreysobol.com>
2019-09-07 11:01:04 +0300

    while.sol

---
+++ solidity-contracts/while.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.11;
+
+contract Summator {
+  uint public value;
+  
+  function whiler() public returns (uint yourMom) {
+    uint y = 0;
+    while (y != 2) {
+        y += 1;
+    }
+    return y;
+  }
+}

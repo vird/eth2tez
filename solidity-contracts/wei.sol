4347fb7137d287123d688a41c68760f6fe6f8b80
Julian Konchunas <konchunas@gmail.com>
2019-09-08 09:33:32 +0300

    Wei example

---
+++ solidity-contracts/wei.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.11;
+
+contract Summator {
+  uint256 public value = 2 wei;
+  
+  function sum() public returns (uint yourMom) {
+    uint256 x = 5 wei;
+    return value + x;
+  }
+}
